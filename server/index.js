4bb6c45b2df1d50ba8708143eb5f011e08c4fead
Myles Borins <mborins@us.ibm.com>
2015-12-15 14:12:30 -0800

    Update with custom server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var path = require('path');
+
+var express = require('express');
+var compression = require('compression');
+
+var app = express();
+var server = http.Server(app);
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+server.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
